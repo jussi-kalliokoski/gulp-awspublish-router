cd209b0900259f7285d6c53f9daf01f1f1188a0f
spencerbeggs <spencer@beg.gs>
2018-10-15 12:54:47 -0400

    Use previous code formatiing

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,17 @@ var gulp = require("gulp");
 var mocha = require("gulp-mocha");
 var istanbul = require("gulp-istanbul");
 
-gulp.task("mocha", function(callback) {
-  return gulp
-    .src(["./index.js", "./lib/**/*.js"])
-    .pipe(istanbul())
-    .on("finish", function() {
-      gulp
-        .src(["test/*.js"])
-        .pipe(
-          mocha({
-            reporter: "spec"
-          })
-        )
-        .pipe(istanbul.writeReports())
-        .on("end", callback);
-    });
+gulp.task("mocha", function (callback) {
+    gulp.src(["./index.js", "./lib/**/*.js"])
+        .pipe(istanbul())
+        .on("finish", function () {
+            gulp.src(["test/*.js"])
+                .pipe(mocha({
+                    reporter: "spec"
+                }))
+                .pipe(istanbul.writeReports())
+                .on("end", callback);
+        });
 });
 
 gulp.task("test", gulp.series("mocha"));
